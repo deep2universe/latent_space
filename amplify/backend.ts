f3c60250def7fc25c1ff7f344520e37d522ef10b
deep2universe <deep2universe@gmail.com>
2025-01-14 14:39:53 +0100

    auth to backends

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { data } from "./data/resource";
 import { storage } from './storage/resource';
+import { auth } from './auth/resource';
 
 defineBackend({
     data,
-    storage
+    storage,
+    auth,
 });
