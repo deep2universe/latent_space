bee043dfa6a7aa5ff6ccd3aaf9c46c24faf9a735
deep2universe <deep2universe@gmail.com>
2025-01-14 13:42:10 +0100

    remve access to storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,11 +1,5 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    name: 'latent_space_drive',
-    access: (allow) => ({
-        'memory_game/*': [
-            allow.authenticated.to(['read','write']),
-            allow.guest.to(['read', 'write'])
-        ],
-    })
+    name: 'latent_space_drive'
 });
