05bb4abd5c72a43bb097336d85dee784a5e33907
deep2universe <deep2universe@gmail.com>
2025-01-14 12:07:13 +0100

    add access to storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,11 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-    name: 'latent_space_drive'
+    name: 'latent_space_drive',
+    access: (allow) => ({
+        'memory_game/*': [
+            allow.authenticated.to(['read','write']),
+            allow.guest.to(['read', 'write'])
+        ],
+    })
 });
