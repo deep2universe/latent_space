13bb576ff067f28770d7b9636bfcbdcbc6b37cf5
deep2universe <deep2universe@gmail.com>
2025-01-14 15:02:55 +0100

    login styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import App from './App.tsx'
-import './index.css'
+import './index.css';
+import './login.css';
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
@@ -17,6 +18,9 @@ Amplify.configure({
  */
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <video autoPlay loop muted>
+      <source src="https://cdn.pixabay.com/video/2021/09/11/88207-602915574_large.mp4" type="video/mp4" />
+    </video>
     <Authenticator signUpAttributes={[]}>
       {({ signOut, user }) => (
         <main>
